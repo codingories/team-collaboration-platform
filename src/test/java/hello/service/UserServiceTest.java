13ac4323725eb8bd3289c5101af5dfd86ec26e1d
codingories <tellt@126.com>
2026-04-23 11:49:07 +0800

    feat: 增加saveUser的test

--- src/test/java/hello/service/UserServiceTest.java
+++ src/test/java/hello/service/UserServiceTest.java
@@ -1,7 +1,39 @@
 package hello.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+    @Mock
+    BCryptPasswordEncoder mockEncoder;
 
+    @Mock
+    UserMapper mockMapper;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    public void testSave() {
+        // ==========================================
+        // ✅ Given（给定）：准备条件、模拟行为
+        // ==========================================
+        Mockito.when(mockEncoder.encode("myPassword")).thenReturn("myEncodedPassword");
+        // ==========================================
+        // ✅ When（当）：执行要测试的方法
+        // ==========================================
+        userService.save("myUser", "myPassword");
+        // ==========================================
+        // ✅ Then（然后）：验证结果是否正确
+        // ==========================================
+//        断言：必须调用过一次，参数是 ("myUser", "myPassword")
+        Mockito.verify(mockMapper).save("myUser", "myEncodedPassword");
+    }
 }
